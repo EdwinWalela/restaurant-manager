b34b413650df915a0719cb4f2236a8c7f14d9cf5
edwinwalela <edwinwalela@gmail.com>
2019-07-18 17:08:49 +0300

    header styles within component

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,13 +5,35 @@ import UserOrders from '../UserOrders';
 
 function Header(props) {
     return (
-        <div className="header">
-        <i class="fas fa-bars" onClick={props.handleUserOrderDisplay}></i>
-            <h1>{props.title}</h1>
-
+        <div style={headerStyle}>
+            <i className="fa fa-bars" style={orderButtonStyle} onClick={props.handleUserOrderDisplay}></i>
+            <h1 style={titleStyle}>{props.title}</h1>
             {props.userOrderDisplay ? <UserOrders items={props.userOrder} /> : ''}
         </div>
     )
+}
+
+const headerStyle = {
+    color:'#fff',
+    background:'#FF1053',
+    top:'0',
+    position: 'fixed',
+    width: '100%'
+}
+
+const titleStyle = {
+    padding:'20px',
+    textAlign: 'center'
+}
+
+const orderButtonStyle = {
+    display:'block',
+    margin:'10px',
+    fontSize: '2em',
+    color:'#333',
+    padding:'5px',
+    float: 'right',
+    transition:'all linear .1s'
 }
 
 Header.propTypes = {
