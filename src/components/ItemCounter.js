723d29f4fa1cb58d927473cf00250760cd6d9152
edwinwalela <edwinwalela@gmail.com>
2019-07-18 19:27:29 +0300

    itemCounter component styles

--- src/components/ItemCounter.js
+++ src/components/ItemCounter.js
@@ -2,24 +2,48 @@ import React from 'react'
 import PropTypes from 'prop-types'
 
 function ItemCounter(props) {
-
     let itemCount = props.handleUserOrderCount(props.itemId)
-
     return (
-        <div className="item-counter">
-            {itemCount > 0 ? <span>{itemCount}</span> :''}
+        <div style={itemCounterStyle}>
+            {itemCount > 0 ? <span style={itemCountStyle}>{itemCount}</span> :''}
            <button onClick={props.handleUserOrderChange.bind(this,props.itemId,1)} style={increaseBtnStyle}>+</button>
            <button onClick={props.handleUserOrderChange.bind(this,props.itemId,0)} style={reduceBtnStyle}>-</button>
         </div>
     )
 }
 
+const itemCounterStyle = {
+    fontSize: '0.85em',
+    padding:'0 5px',
+    textAlign:'right'
+}
+
+const itemCountStyle = {
+    color:'#333',
+    background:'#fff',
+    borderRadius:'5px',
+    padding:'5px 10px',
+    fontSize:'1.1em',
+    fontWeight:'600',
+    margin:'10px'
+}
+
+const buttonStyle = {
+    color:'#fff',
+    padding:'5px',
+    fontSize: '1.2em',
+    width:'50px',
+    border:'solid 1px transparent',
+}
+
 const reduceBtnStyle = {
-    background:'#333'
+    background:'#333',
+    ...buttonStyle
 }
 
 const increaseBtnStyle = {
-    background:'#FF1053'
+    background:'#FF1053',
+    ...buttonStyle
 }
 
 ItemCounter.propTypes = {
