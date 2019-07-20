0deb07cc33e238e83a7a1d6dd2733cde7430f45f
edwinwalela <edwinwalela@gmail.com>
2019-07-20 21:27:10 +0300

    convert progressbar to class-component

--- client/src/components/admin/ProgressBar.js
+++ client/src/components/admin/ProgressBar.js
@@ -1,21 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
-function ProgressBar(props) {
-    return (
-        <div style={progressBarstyle}>
-            <span>0</span>
-            <span>1</span>
-            <span>2</span>
-            <span>3</span>
-            <span>4</span>
-        </div>
-    )
+
+class ProgressBar extends Component {
+    render(){
+        return (
+            <div style={progressBarstyle}>
+                <span style={levelStyle}>0</span>
+                <span style={levelStyle}>1</span>
+                <span style={levelStyle}>2</span>
+                <span style={levelStyle}>3</span>
+                <span style={levelStyle}>4</span>
+            </div>
+        )
+    }
 }
 
 const progressBarstyle = {
-
+    textAlign:'center',
+    margin:'20px auto 20px 120px',
+    width:'400px',
+    background:'#eee',
+    borderRadius:'5px',
 }
+
+const levelStyle = {
+    display:'inline-block',
+    padding:'5px',
+    borderRadius:'5px',
+    width:'20%',
+    background:'#eee',
+}
+
+
 
 ProgressBar.propTypes = {
 
