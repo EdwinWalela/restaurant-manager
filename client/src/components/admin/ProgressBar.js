405abd0bdcc95451a654471c6946637e3813c3a9
edwinwalela <edwinwalela@gmail.com>
2019-07-20 21:38:46 +0300

    progress bar styling

--- client/src/components/admin/ProgressBar.js
+++ client/src/components/admin/ProgressBar.js
@@ -3,14 +3,40 @@ import PropTypes from 'prop-types'
 
 
 class ProgressBar extends Component {
+
+    levelStyle = (level) =>{
+        let style = {
+            display:'inline-block',
+            padding:'4px',
+            borderRadius:'5px',
+            width:'20%',
+            background:'#eee',
+            color:'#fff',
+            fontSize:'0.8em'
+        }
+
+        if(level === 0){
+            style.background = '#FE5F55';
+        }else if(level === 1){
+            style.background = '#507DBC';
+        }else if(level === 2){
+            style.background = 'orange';
+        }else if(level === 3){
+            style.background = '#FFBA08';
+        }else{
+            style.background = '#5FBB97';
+        }
+        return style;
+    }
+
     render(){
         return (
             <div style={progressBarstyle}>
-                <span style={levelStyle}>0</span>
-                <span style={levelStyle}>1</span>
-                <span style={levelStyle}>2</span>
-                <span style={levelStyle}>3</span>
-                <span style={levelStyle}>4</span>
+                <span style={this.levelStyle(0)}>Pending</span>
+                <span style={this.levelStyle(1)}>Preparation</span>
+                <span style={this.levelStyle(2)}>In Oven</span>
+                <span style={this.levelStyle(3)}>Check</span>
+                <span style={this.levelStyle(4)}>Ready</span>
             </div>
         )
     }
@@ -20,19 +46,9 @@ const progressBarstyle = {
     textAlign:'center',
     margin:'20px auto 20px 120px',
     width:'400px',
-    background:'#eee',
+    background:'',
     borderRadius:'5px',
 }
-
-const levelStyle = {
-    display:'inline-block',
-    padding:'5px',
-    borderRadius:'5px',
-    width:'20%',
-    background:'#eee',
-}
-
-
 
 ProgressBar.propTypes = {
 
