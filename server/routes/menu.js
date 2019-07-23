1fc45ab3f12ad3705226d4c735bcd78604a0af54
edwinwalela <edwinwalela@gmail.com>
2019-07-23 17:23:19 +0300

    update item

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -41,6 +41,14 @@ router.post('/',async(req,res)=>{
     console.log(req.body)
     let body = req.body;
 
+    // Assume body is empty if first attribute is not pressent
+    if(!body.type){
+        res.status(300).send({
+            error:"empty body"
+        });
+        return;
+    }
+
     let newItem = {
         type:body.type,
         name:body.name,
@@ -62,6 +70,40 @@ router.post('/',async(req,res)=>{
         msg:'new item created'
     })
 
+})
+
+router.put('/:id',async(req,res)=>{
+    let itemId = req.params.id;
+    let updatedItem = req.body;
+
+    if(!itemId){
+        res.status(300).send({
+            error:'id missing'
+        })
+        return;
+    }
+    if(!updatedItem.type){
+        res.status(300).send({
+            error:'body missing'
+        })
+        return;
+    }
+    try {
+        await MenuItems.findByIdAndUpdate(itemId,{
+            type:updatedItem.type,
+            name:updatedItem.name,
+            price:updatedItem.price,
+        })
+    } catch (err) {
+        res.status(500).send({
+            error:err.message
+        })
+        return;
+    }
+
+    res.json({
+        msg:'item updated'
+    })
 })
 
 module.exports = router;
