e3b6a9a6ccdc1c4976f7892f35b6cbc1fb4ac597
edwinwalela <edwinwalela@gmail.com>
2019-07-23 16:50:17 +0300

    menuItem by ID GET endpoint

--- server/routes/menu.js
+++ server/routes/menu.js
@@ -20,4 +20,23 @@ router.get('/',async(req,res,next)=>{
     })
 })
 
+router.get('/:id',async(req,res)=>{
+    let itemId = req.params.id;
+    let menuItem;
+
+    try{
+        menuItem = await MenuItems.findById(itemId);
+    }
+    catch(err){
+        res.status(500).send({
+            error:err.message
+        });
+        return;
+    }
+
+    res.json({
+        menuItem
+    })
+})
+
 module.exports = router;
